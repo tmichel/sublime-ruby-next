206870eba3ef98e2713f5f40d706d235c31deb52
Tamas Michelberger <tomi@secretsaucepartners.com>
2015-06-23 00:14:22 +0200

    Add monokai theme and setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -s "$PWD/Ruby Next.tmLanguage" "$PWD/Ruby on Rails Next.tmLanguage" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+ln -s "$PWD/MyMonokai.tmTheme" "$HOME/Library/Application Support/Sublime Text 3/Packages"
+
