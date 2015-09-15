e93ca14efb99adcff0f2aff75b6f46952f3c05f6
Tamas Michelberger <tomi@secretsaucepartners.com>
2015-09-15 11:54:37 +0200

    Move Ruby and Rails related files to their own directories

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ln -s "$PWD/Ruby Next.tmLanguage" "$PWD/Ruby on Rails Next.tmLanguage" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+ln -s "$PWD/Ruby" "$PWD/Rails" "$HOME/Library/Application Support/Sublime Text 3/Packages"
 ln -s "$PWD/MyMonokai.tmTheme" "$HOME/Library/Application Support/Sublime Text 3/Packages"
 
