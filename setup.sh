5261ccb38986073d9e1c44557e6a0b17cc014f83
Tamás Michelberger <tomi@secretsaucepartners.com>
2016-01-24 22:31:03 +0100

    Add support for checking what needs to be installed

--- setup.sh
+++ setup.sh
@@ -2,5 +2,25 @@
 
 PACKAGES_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages"
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+FILES=(
+  "$DIR/Ruby"
+  "$DIR/Rails"
+  "$DIR/MyMonokai.tmTheme"
+)
 
-ln -Fs "$DIR/Ruby" "$DIR/Rails" "$DIR/MyMonokai.tmTheme" "$PACKAGES_DIR"
+check() {
+  local path
+  for file in ${FILES[*]}; do
+    path="${file/$DIR/$PACKAGES_DIR}"
+    if [ ! -L "$path" ]; then
+      echo $file
+    fi
+  done
+}
+
+if [ "$1" = "--check" ]; then
+  check
+else
+  ln -Fs "${FILES[@]}" "$PACKAGES_DIR"
+fi
+
