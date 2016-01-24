226872c6ee5197764ea36697d432cee758fb47b7
Tamás Michelberger <tomi@secretsaucepartners.com>
2016-01-24 15:23:56 +0100

    Make setup script not to depend on pwd

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-ln -s "$PWD/Ruby" "$PWD/Rails" "$HOME/Library/Application Support/Sublime Text 3/Packages"
-ln -s "$PWD/MyMonokai.tmTheme" "$HOME/Library/Application Support/Sublime Text 3/Packages"
+PACKAGES_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages"
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+ln -Fs "$DIR/Ruby" "$DIR/Rails" "$DIR/MyMonokai.tmTheme" "$PACKAGES_DIR"
